b4528c1e65f242632b4c4a90d5cfb44a35a39993
ayowassup <afridhaberliani@gmail.com>
2018-05-05 09:45:00 +0700

    Fragment added, but function still getting error

---
+++ app/src/main/java/squad/seven/bayarkas/Fragment/FragmentSettings.java
@@ -0,0 +1,32 @@
+package squad.seven.bayarkas.Fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import squad.seven.bayarkas.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FragmentSettings extends Fragment {
+
+
+    public FragmentSettings() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_settings, container, false);
+
+        return v;
+    }
+
+}
