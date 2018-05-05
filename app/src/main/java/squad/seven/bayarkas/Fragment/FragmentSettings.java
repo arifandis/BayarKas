5ff9563263c381c6de94497344c597d87921ebf0
arifandis <arifandiswinata@gmail.com>
2018-05-05 16:20:59 +0700

    Ngerapiin layout

--- app/src/main/java/squad/seven/bayarkas/Fragment/FragmentSettings.java
+++ app/src/main/java/squad/seven/bayarkas/Fragment/FragmentSettings.java
@@ -3,9 +3,11 @@ package squad.seven.bayarkas.Fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import squad.seven.bayarkas.R;
 
@@ -13,6 +15,7 @@ import squad.seven.bayarkas.R;
  * A simple {@link Fragment} subclass.
  */
 public class FragmentSettings extends Fragment {
+    Button mTambahData,mEdit;
 
 
     public FragmentSettings() {
@@ -26,7 +29,26 @@ public class FragmentSettings extends Fragment {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_settings, container, false);
 
+        mTambahData = v.findViewById(R.id.tambah_data);
+        mEdit = v.findViewById(R.id.edit);
+
+        mTambahData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                moveFragment(mTambahData);
+            }
+        });
+
         return v;
     }
 
+    void moveFragment(View v){
+        Fragment fragment=null;
+        if (v.equals(mTambahData)){
+            fragment = new FragmentTambahData();
+        }
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        transaction.replace(R.id.content_view,fragment);
+        transaction.commit();
+    }
 }
--- app/src/main/java/squad/seven/bayarkas/Fragment/FragmentSimpan.java
+++ app/src/main/java/squad/seven/bayarkas/Fragment/FragmentSimpan.java
@@ -24,7 +24,9 @@ public class FragmentSimpan extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_simpan, container, false);
+        View v = inflater.inflate(R.layout.fragment_simpan, container, false);
+
+        return v;
     }
 
 }
