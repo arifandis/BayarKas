50b50aef8f36c36eb8eeb3f5f29b2b34fd740a93
ayowassup <afridhaberliani@gmail.com>
2018-05-05 13:51:28 +0700

    RecyclerView Added, but with dummy data. No database inserted.

--- app/src/main/java/squad/seven/bayarkas/Fragment/FragmentRekap.java
+++ app/src/main/java/squad/seven/bayarkas/Fragment/FragmentRekap.java
@@ -3,17 +3,25 @@ package squad.seven.bayarkas.Fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import squad.seven.bayarkas.R;
+import squad.seven.bayarkas.RekapitulasiDetail;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class FragmentRekap extends Fragment {
-
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private List<RekapitulasiDetail> listitem;
 
     public FragmentRekap() {
         // Required empty public constructor
@@ -25,6 +33,24 @@ public class FragmentRekap extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_rekap, container, false);
+
+        recyclerView = v.findViewById(R.id.recycler_list_rekap);
+        recyclerView.setNestedScrollingEnabled(false);
+        recyclerView.setHasFixedSize(true);
+
+        LinearLayoutManager linearLayout = new LinearLayoutManager(this.getActivity());
+        recyclerView.setLayoutManager(linearLayout);
+        linearLayout.setStackFromEnd(true);
+
+        listitem = new ArrayList<>();
+        for (int i = 0; i<100; i++){
+            RekapitulasiDetail listitems = new RekapitulasiDetail("namaorang"+(i+1), "1000"+(i+1));
+            listitem.add(listitems);
+            }
+
+        adapter = new RekapAdapter(listitem, this.getActivity());
+        recyclerView.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
 
         return v;
     }
--- app/src/main/java/squad/seven/bayarkas/Fragment/ViewHistoryFragment.java
+++ app/src/main/java/squad/seven/bayarkas/Fragment/ViewHistoryFragment.java
@@ -3,29 +3,49 @@ package squad.seven.bayarkas.Fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import squad.seven.bayarkas.HistoryKas;
 import squad.seven.bayarkas.R;
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class ViewHistoryFragment extends Fragment {
-
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private List<HistoryKas> listitem;
 
     public ViewHistoryFragment() {
         // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_view_history, container, false);
+        recyclerView = v.findViewById(R.id.recycler_list_history);
+        recyclerView.setNestedScrollingEnabled(false);
+        recyclerView.setHasFixedSize(true);
 
+        LinearLayoutManager linearLayout = new LinearLayoutManager(this.getActivity());
+        recyclerView.setLayoutManager(linearLayout);
+        linearLayout.setStackFromEnd(true);
+
+        listitem = new ArrayList<>();
+        for (int i = 0; i<100; i++){
+            HistoryKas listitems = new HistoryKas("namaorang"+(i+1), "1000"+(i+1));
+            listitem.add(listitems);
+        }
+
+        adapter = new ViewHistoryAdapter(listitem, this.getActivity());
+        recyclerView.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
         return v;
     }
 
